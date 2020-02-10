96a4e8ffa5351d43c49cff108799ce74a55e3bb4
hoangquandinh2109 <34148141+hoangquandinh2109@users.noreply.github.com>
2020-02-10 11:05:20 +0700

    danhmuc: chucvu, dotthu

---
+++ src/app/views/danhmuc/chucvu/chucvu.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { Filter } from '../../../models/filter';
+import { UtilsService } from '../../../services/utils.service';
+import { Router } from '@angular/router';
+import { ConfirmComponent } from '../../../shared/modal/confirm/confirm.component';
+import { NgbModal, NgbModalConfig } from '@ng-bootstrap/ng-bootstrap';
+
+import { ChucvuService } from 'app/services/danhmuc/chucvu.service';
+import { Chucvu } from 'app/models/danhmuc/Chucvu';
+import { ChucvuaddoreditComponent } from './chucvuaddoredit/chucvuaddoredit.component';
+
+@Component({
+  selector: 'app-chucvu',
+  templateUrl: './chucvu.component.html',
+  styleUrls: ['./chucvu.component.scss']
+})
+export class ChucvuComponent implements OnInit {
+  chucvuList:Chucvu[] = [];
+  chucvu: Chucvu;
+  searchTerm:string = '';
+  pageIndex:number = 1;
+  pageSize:number = 20;
+  constructor(public util: UtilsService, config: NgbModalConfig, private service: ChucvuService, private router: Router, private modalService: NgbModal) {
+    config.backdrop = 'static';
+    config.keyboard = false;
+    config.scrollable = false;
+   }
+
+  ngOnInit() {
+    this.reload();
+  }
+
+  remove(chucvu: Chucvu) {
+    this.chucvu = chucvu;
+    const _this = this;
+    const modalRef = this.modalService.open(ConfirmComponent, { size: 'lg' });
+    modalRef.componentInstance.confirmObject = 'Postition';
+    modalRef.componentInstance.decide.subscribe(() => {
+	_this.service.deleteChucvu(chucvu.Id);
+        _this.reload();
+    });
+  }
+  reload() {
+    const filter: Filter = new Filter(this.searchTerm, this.pageIndex, this.pageSize);
+    this.chucvuList = this.service.getChucvuList(filter);
+  }
+  add() {
+    this.edit(null);
+  }
+
+  edit(chucvuId: null | number) {
+    const _this = this;
+    const modalRef = this.modalService.open(ChucvuaddoreditComponent, { size: 'lg' });
+    modalRef.componentInstance.popup = true;
+    modalRef.componentInstance.chucvuId = chucvuId;
+    modalRef.result.then(function(result) {
+        _this.reload();
+    });
+  }
+}
---
+++ src/app/views/danhmuc/chucvu/chucvuaddoredit/chucvuaddoredit.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit, ViewEncapsulation, Input, Output, ViewChild, EventEmitter} from '@angular/core';
+import { NgbActiveModal, NgbModal, NgbModalConfig } from '@ng-bootstrap/ng-bootstrap';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Chucvu } from 'app/models/danhmuc/Chucvu';
+import { ChucvuService } from 'app/services/danhmuc/chucvu.service';
+import { ConfirmComponent } from '../../../../shared/modal/confirm/confirm.component';
+@Component({
+  selector: 'app-chucvuaddoredit',
+  templateUrl: './chucvuaddoredit.component.html',
+  styleUrls: ['./chucvuaddoredit.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class ChucvuaddoreditComponent implements OnInit {
+
+	@Input() popup: boolean;
+	@Input() chucvuId: number;
+	@Output() capNhatThanhCong: EventEmitter<any> = new EventEmitter();
+
+	chucvu: Chucvu = new Chucvu(0,'','',0);
+
+	constructor(public activeModal: NgbActiveModal,private chucvuService: ChucvuService, config: NgbModalConfig , private modalService: NgbModal, private route: ActivatedRoute, private router: Router) {
+		this.chucvuId = this.route.snapshot.queryParams['Id'];
+		this.chucvuId = (this.chucvuId) ? this.chucvuId : 0;
+		config.backdrop = 'static';
+     		config.keyboard = false;
+		config.scrollable = false;
+	}  
+	GetChucvuById(chucvuId:number)  
+	{  
+		this.chucvu = this.chucvuService.getChucvu(chucvuId);
+		if (this.chucvu == null || this.chucvu.Id == 0) {
+			this.chucvu = new Chucvu(0,'','',0);
+		}
+	}
+	ngOnInit() {
+		this.GetChucvuById(this.chucvuId);  
+	}
+
+	ReturnList() {
+		this.router.navigate(['danhmuc/chucvu']); 
+
+	}
+
+	UpdateChucvu() {
+		const result: boolean = this.chucvuService.addOrUpdateChucvu(this.chucvu);
+		if (result) {
+			if(!this.popup) {
+				this.ReturnList();
+			} else {
+				this.closeMe();
+			}
+		} else {
+			const modalRef = this.modalService.open(ConfirmComponent, { size: 'lg' });
+		}
+	}
+	
+	closeMe() {
+		this.activeModal.close();
+	}
+
+}

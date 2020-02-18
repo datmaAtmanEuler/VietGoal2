7bee4c72d5d19acc703b5aee9395dfd27917acca
hoangquandinh2109 <34148141+hoangquandinh2109@users.noreply.github.com>
2020-02-18 15:10:21 +0700

    better

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -2,37 +2,43 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 declare const $: any;
 declare interface RouteInfo {
-    path: string;
-    title: string;
-    icon: string;
-    class: string;
-    childrens?: RouteInfo[];
+  path: string;
+  title: string;
+  icon: string;
+  class: string;
+  childrens?: RouteInfo[];
 }
 export const ROUTES: RouteInfo[] = [
-  { path: '/dashboard', title: 'Dashboard',  icon: 'dashboard', class: '' },
-   { path: '/controlmanagement/categories', title: 'MESSAGE.NameList.ControlManagement',  icon: 'view_list', class: '', childrens: [
-  { path: '/controlmanagement/categories', title: 'MESSAGE.NameList.Catalogs',  icon: '', class: '' }
-  ] },
-  { path: '/danhmuc/tinhthanh', title: 'MESSAGE.NameList.Catalogs',  icon: 'view_list', class: '', childrens: [
-  { path: '/danhmuc/tinhthanh', title: 'MESSAGE.NameList.ProvincesList',  icon: '', class: '' },
-  { path: '/danhmuc/quanhuyen', title: 'MESSAGE.NameList.DistrictsList',  icon: '', class: '' },
-  { path: '/danhmuc/phuongxa', title: 'MESSAGE.NameList.CommunesWardsList',  icon: '', class: '' },
-  { path: '/danhmuc/nhomnguoidung', title: 'MESSAGE.NameList.AdministrationOfUserGroups',  icon: '', class: '' },
-{ path: '/danhmuc/santap', title: 'MESSAGE.NameList.ManagingTrainingGround',  icon: '', class: '' },
-  { path: '/danhmuc/chucvu', title: 'MESSAGE.NameList.PositionsList',  icon: '', class: '' },
-  { path: '/danhmuc/trangthailophoc', title: 'MESSAGE.NameList.ClassStatusList',  icon: '', class: '' },
-  { path: '/danhmuc/trangthaihocvien', title: 'MESSAGE.NameList.StudentStatusList',  icon: '', class: '' },
-  { path: '/danhmuc/trangthaihlv', title: 'MESSAGE.NameList.CoachStatusList',  icon: '', class: '' },
-  { path: '/danhmuc/dotthu', title: 'MESSAGE.NameList.TermOfCollectionsList',  icon: '', class: '' },
-  { path: '/danhmuc/khoanthu', title: 'MESSAGE.NameList.CollectionsList',  icon: '', class: '' },
-  { path: '/danhmuc/luatuoi', title: 'MESSAGE.NameList.AgesList',  icon: '', class: '' },
-  { path: '/danhmuc/baitap', title: 'MESSAGE.NameList.YardArea',  icon: '', class: '' },
-  { path: '/danhmuc/khuvuc', title: 'MESSAGE.NameList.RegionsList',  icon: '', class: '' }
-  ] },
-{ path: '/quanly/trungtam', title: 'MESSAGE.NameList.Manage',  icon: 'assignment_ind', class: '', childrens: [
-{ path: '/quanly/trungtam', title: 'MESSAGE.NameList.CentralManagement',  icon: '', class: '' },  
-{ path: '/quanly/thoikhoabieu', title: 'MESSAGE.NameList.Schedule',  icon: '', class: '' }
-  ] }
+  { path: '/dashboard', title: 'Dashboard', icon: 'dashboard', class: '' },
+  {
+    path: '/controlmanagement/categories', title: 'MESSAGE.NameList.ControlManagement', icon: 'view_list', class: '', childrens: [
+      { path: '/controlmanagement/categories', title: 'MESSAGE.NameList.Catalogs', icon: '', class: '' }
+    ]
+  },
+  {
+    path: '/danhmuc/tinhthanh', title: 'MESSAGE.NameList.Catalogs', icon: 'view_list', class: '', childrens: [
+      { path: '/danhmuc/tinhthanh', title: 'MESSAGE.NameList.ProvincesList', icon: '', class: '' },
+      { path: '/danhmuc/quanhuyen', title: 'MESSAGE.NameList.DistrictsList', icon: '', class: '' },
+      { path: '/danhmuc/phuongxa', title: 'MESSAGE.NameList.CommunesWardsList', icon: '', class: '' },
+      { path: '/danhmuc/nhomnguoidung', title: 'MESSAGE.NameList.AdministrationOfUserGroups', icon: '', class: '' },
+      { path: '/danhmuc/santap', title: 'MESSAGE.NameList.ManagingTrainingGround', icon: '', class: '' },
+      { path: '/danhmuc/chucvu', title: 'MESSAGE.NameList.PositionsList', icon: '', class: '' },
+      { path: '/danhmuc/trangthailophoc', title: 'MESSAGE.NameList.ClassStatusList', icon: '', class: '' },
+      { path: '/danhmuc/trangthaihocvien', title: 'MESSAGE.NameList.StudentStatusList', icon: '', class: '' },
+      { path: '/danhmuc/trangthaihlv', title: 'MESSAGE.NameList.CoachStatusList', icon: '', class: '' },
+      { path: '/danhmuc/dotthu', title: 'MESSAGE.NameList.TermOfCollectionsList', icon: '', class: '' },
+      { path: '/danhmuc/khoanthu', title: 'MESSAGE.NameList.CollectionsList', icon: '', class: '' },
+      { path: '/danhmuc/luatuoi', title: 'MESSAGE.NameList.AgesList', icon: '', class: '' },
+      { path: '/danhmuc/baitap', title: 'MESSAGE.NameList.YardArea', icon: '', class: '' },
+      { path: '/danhmuc/khuvuc', title: 'MESSAGE.NameList.RegionsList', icon: '', class: '' }
+    ]
+  },
+  {
+    path: '/quanly/trungtam', title: 'MESSAGE.NameList.Manage', icon: 'assignment_ind', class: '', childrens: [
+      { path: '/quanly/trungtam', title: 'MESSAGE.NameList.CentralManagement', icon: '', class: '' },
+      { path: '/quanly/thoikhoabieu', title: 'MESSAGE.NameList.Schedule', icon: '', class: '' }
+    ]
+  }
 ];
 
 @Component({
@@ -47,38 +53,38 @@ export class SidebarComponent implements OnInit {
   dropParent: boolean[] = [];
   @Output('sidebarUpdate') sidebarUpdate = new EventEmitter<boolean>();
 
-  constructor() { 
+  constructor() {
   }
 
   ngOnInit() {
     this.menuItems = ROUTES.filter(menuItem => menuItem);
     this.dropParent = [];
     const _this = this;
-    this.menuItems.forEach(function(mn: any){
-	   _this.dropParent.push(false);
+    this.menuItems.forEach(function (mn: any) {
+      _this.dropParent.push(false);
     });
 
   }
   isMobileMenu() {
-      if ($(window).width() > 991) {
-          return false;
-      }
-      return true;
+    if ($(window).width() > 991) {
+      return false;
+    }
+    return true;
   };
 
-  toggle(){
-	 this.mini = !this.mini;
-   this.sidebarUpdate.emit(this.mini);
+  toggle() {
+    this.mini = !this.mini;
+    this.sidebarUpdate.emit(this.mini);
   }
 
   xulyDropParents(index: number) {
-  	const _this = this;
-  	this.dropParent.forEach(function(dp: any, ind: number){
-  	   if(ind != index) {   	
-  	   	_this.dropParent[ind] = false;
-  	   } else {
-  	   	_this.dropParent[ind] = !_this.dropParent[ind];
-  	   }
-  	});	
+    const _this = this;
+    this.dropParent.forEach(function (dp: any, ind: number) {
+      if (ind != index) {
+        _this.dropParent[ind] = false;
+      } else {
+        _this.dropParent[ind] = !_this.dropParent[ind];
+      }
+    });
   }
 }

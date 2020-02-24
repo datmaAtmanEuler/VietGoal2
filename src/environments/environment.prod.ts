dc59750197ea0fcdddbed3fb4c42548bcfb4e5eb
ngdanghuy0612 <36077650+ngdanghuy0612@users.noreply.github.com>
2020-02-24 18:37:38 +0700

    u

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,8 @@
 export const environment = {
   production: true,
-  serverOriginUrl: 'https://localhost:44353/',
-  serverUrl: 'https://localhost:44353/Api/',
-  serverUrl_employee: 'https://localhost:44353/Api/'
+  serverOriginUrl: 'https://localhost:44349/',
+  serverUrl: 'https://localhost:44349/Api/',
+  serverUrl_employee: 'https://localhost:44349/Api/',
+  apiUrl: 'https://localhost:44353/api/',
+  apiOriginalUrl: 'https://localhost:44353/'
 };
